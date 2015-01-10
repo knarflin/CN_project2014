68019a2497964e7f84e3d7359c36bf96dcb44adb
Yiming Luo <lym9503@gmail.com>
2015-01-10 11:41:45 +0800

    1. Add struct "account" 2. Add "jobqueue" into struct "account"

--- functions/function_test.c
+++ functions/function_test.c
@@ -3,7 +3,7 @@
 #include"parse.h"
 #include"filter.h"
 #include"account.h"
-
+#include"cqueue.h"
 
 //---------------------------- parse_test1 ----------------------------
 void parse_test1(){
@@ -55,58 +55,105 @@ void filter_test(){
 }
 
 //---------------------------- account_test() ----------------------------
-void account_test(){
-	accountcnt=0;
+void account_test(){ 
 	int i;
-
+	accountcnt = 0;
+	
 	i=create_account("Alice","lovely");
 	printf("Creating account Alice...\n");
 	if(i==0) printf("Successful!\n");
-	else if(i==1) printf("Username already existed!\n");
+	else if(i==1) printf("Username already exists!\n");
 	printf("\n");
 
 	i=create_account("Bob","crispy");
 	printf("Creating account Bob...\n");
 	if(i==0) printf("Successful!\n");
-	else if(i==1) printf("Username already existed!\n");
+	else if(i==1) printf("Username already exists!\n");
 	printf("\n");
 
 	i=create_account("Alice","happy");
 	printf("Creating account Alice...\n");
 	if(i==0) printf("Successful!\n");
-	else if(i==1) printf("Username already existed!\n");
+	else if(i==1) printf("Username already exists!\n");
 	printf("\n");
 
 	i=authenticate("Alice","creepy");
 	printf("Logging in account \"Alice\"...\n");
 	if(i==0) printf("Correct password!\n");
 	else if(i==1) printf("Wrong password!\n");
-	else if(i==2) printf("Username not existed!\n");
+	else if(i==2) printf("Username not exist!\n");
 	printf("\n");
 
 	i=authenticate("Bob","crispy");
 	printf("Logging in account \"Bob\"...\n");
 	if(i==0) printf("Correct password!\n");
 	else if(i==1) printf("Wrong password!\n");
-	else if(i==2) printf("Username not existed!\n");
+	else if(i==2) printf("Username not exist!\n");
 	printf("\n");
 
 	i=authenticate("Cathy","sweety");
 	printf("Logging in account \"Cathy\"...\n");
 	if(i==0) printf("Correct password!\n");
 	else if(i==1) printf("Wrong password!\n");
-	else if(i==2) printf("Username not existed!\n");
+	else if(i==2) printf("Username not exist!\n");
 	printf("\n");
 
-	print_account();
-
+	print_account(); 
 }
+
+// ---------------------------- jobqueue_test() ----------------------------
+void jobqueue_test(){
+	int i;
+	struct job jb[10]; // an array saving jobs
+	struct job* jptr;
+
+	// create jobs
+	job_assign(&jb[0],"Alice",'m',0,NULL,"Good night!");
+	job_assign(&jb[1],"Bob",'f',12,"autorun.exe","Hello!");
+	job_assign(&jb[2],"Cathy",'m',0,NULL,"Oh!");
+	job_assign(&jb[3],"Derrick",'f',0,"angry_bird.exe","Ah?");
+
+	// initialize job queues
+	for(i=0;i<=2;i++)
+		queue_init(&accountinfo[i].job_queue);
+
+	// assign each job to some account
+	enqueue(&accountinfo[0].job_queue,&jb[0]);
+	enqueue(&accountinfo[0].job_queue,&jb[3]);
+	enqueue(&accountinfo[1].job_queue,&jb[1]);
+	enqueue(&accountinfo[2].job_queue,&jb[2]);
+
+	// print job queue of each account
+	for(i=0;i<4;i++){
+		printf("----- Job queue of account #%d -----\n",i);
+		print_queue(&accountinfo[i].job_queue);
+		printf("\n");
+	}
+
+	// dequeue a job from job queue of account 1 
+	printf("---Dequeue a job from job queue of account 1---\n");
+	i=dequeue(&accountinfo[1].job_queue,&jptr); 
+	if(i==0)
+		print_job(jptr);
+	else if(i==1)
+		printf("Job queue is empty!\n");
+	printf("\n");
+
+	// dequeue a job from job queue of account 1 again. This time, the queue is empty.
+	printf("---Dequeue a job from job queue of account 1 again---\n");
+	i=dequeue(&accountinfo[1].job_queue,&jptr); 
+	if(i==0)
+		print_job(jptr);
+	else if(i==-1)
+		printf("Job queue is empty!\n");
+};
 
 int main()
 {
 	//parse_test1();
 	//parse_test2();
 	//filter_test();
-	account_test(); // replace it with the function you want to test
+	//account_test();
+	jobqueue_test(); // replace it with the function you want to test
 	return 0;
 }
